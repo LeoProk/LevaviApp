b186f39eeab75a9dcf79d260314f30cac0f244fb
Leo <just_beta@hotmail.com>
2015-12-08 06:35:21 +0200

    Done Some Work

--- app/src/main/java/org/levavi/levaviapp/MainActivity.java
+++ app/src/main/java/org/levavi/levaviapp/MainActivity.java
@@ -7,9 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout mSearchBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         //sets toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         UIFactory.getToolbar(this, toolbar).doTask();
+        mSearchBox = (RelativeLayout)findViewById(R.id.search_bar);
     }
 
     @Override
@@ -25,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                showSearchBox();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showSearchBox(){
+        mSearchBox.setVisibility(View.VISIBLE);
     }
 }
