f13c942deacbe54851ae02921e1ee605f194ffa3
Leonid Prokopenko <just_beta@hotmail.com>
2015-12-19 22:11:02 +0200

    done some work

---
+++ app/src/main/java/org/levavi/levaviapp/AppSpecifics/CustomNewItemAdapter.java
@@ -0,0 +1,71 @@
+package org.levavi.levaviapp.AppSpecifics;
+
+import org.levavi.levaviapp.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Adapter for list view inside the add new items fragment.
+ */
+public class CustomNewItemAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+
+    private Context mContext;
+
+    private ArrayList<NewItem> mItemsList;
+
+    public CustomNewItemAdapter(Context context,ArrayList itemsList){
+        mContext = context;
+        mItemsList = itemsList;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final NewItem newItem = mItemsList.get(position);
+        if (mInflater == null)
+            mInflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = mInflater.inflate(R.layout.new_item_row, null);
+        //set values of text views and on click event
+        final TextView itemName =(TextView)convertView.findViewById(R.id.item_name);
+        itemName.setText(newItem.getName());
+        final TextView units =(TextView)convertView.findViewById(R.id.units);
+        units.setText(newItem.getUnits());
+        final TextView price =(TextView)convertView.findViewById(R.id.price);
+        price.setText(newItem.getPrice());
+        final ImageButton remove =(ImageButton)convertView.findViewById(R.id.cancel);
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        return  convertView;
+    }
+}
--- app/src/main/java/org/levavi/levaviapp/Fragments/NewItemFragment.java
+++ app/src/main/java/org/levavi/levaviapp/Fragments/NewItemFragment.java
@@ -6,23 +6,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.levavi.levaviapp.AppSpecifics.CustomNewItemAdapter;
+import org.levavi.levaviapp.AppSpecifics.NewItem;
 import org.levavi.levaviapp.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Leo on 12/12/2015.
  */
 public class NewItemFragment extends Fragment {
 
-    TextView mTitle,mAddress,mPhone;
+    private TextView mTitle,mAddress,mPhone,mItem,mUnits,mPrice;
+
+    private CustomNewItemAdapter mItemsAdapter;
+
+    private ArrayList<NewItem> mNewItemsList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_new_item, container, false);
+        //creates array list and adapter and list view
+        final ListView itemsList = (ListView) rootView.findViewById(R.id.items);
+        mNewItemsList = new ArrayList<>();
+        mItemsAdapter = new CustomNewItemAdapter(getActivity(),mNewItemsList);
+        itemsList.setAdapter(mItemsAdapter);
+        //initializes views
         mTitle = (TextView) rootView.findViewById(R.id.title);
         mAddress  = (TextView) rootView.findViewById(R.id.address);
         mPhone  = (TextView) rootView.findViewById(R.id.phone);
@@ -30,12 +45,15 @@ public class NewItemFragment extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //create json and send it to the server
                 createJson();
             }
         });
         return rootView;
     }
 
+
+    //creates json object with all the entered values
     private void createJson(){
         JSONObject object = new JSONObject();
         try {
@@ -45,5 +63,9 @@ public class NewItemFragment extends Fragment {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+    public void updateList(int position){
+        mNewItemsList.remove(position);
+        mItemsAdapter.notifyDataSetChanged();
     }
 }
