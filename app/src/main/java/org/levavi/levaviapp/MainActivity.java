c7871526020145a0159cefccc1bc0031714586e5
Leonid Prokopenko <just_beta@hotmail.com>
2015-12-26 22:15:53 +0200

    done some work

--- app/src/main/java/org/levavi/levaviapp/MainActivity.java
+++ app/src/main/java/org/levavi/levaviapp/MainActivity.java
@@ -19,10 +19,6 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout mSearchBox;
-
-    private boolean mHideSearch;
-
     private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
@@ -33,17 +29,9 @@ public class MainActivity extends AppCompatActivity {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         UtilitiesFactory.getToolbar(this, toolbar).doTask();
         //create the drawer
-        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        final ListView mDrawerList = (ListView) findViewById(R.id.slider_list);
-        mDrawerToggle = (ActionBarDrawerToggle) UtilitiesFactory.getDrawer(this, mDrawerLayout, mDrawerList).doTask();
-        mSearchBox = (RelativeLayout)findViewById(R.id.search_bar);
-        ImageView cancelImg = (ImageView)findViewById(R.id.close);
-        cancelImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchBox(false);
-            }
-        });
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ListView drawerList = (ListView) findViewById(R.id.slider_list);
+        mDrawerToggle = (ActionBarDrawerToggle) UtilitiesFactory.getDrawer(this, drawerLayout, drawerList).doTask();
         //creating star fragment
         UtilitiesFactory.addFragment(this,new ItemsFragment(),"item",true).doTask();
     }
@@ -65,40 +53,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        if(mHideSearch==true){
-            MenuItem item = menu.findItem(R.id.search);
-            item.setVisible(false);
-        }
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.search:
-                searchBox(true);
-                break;
-            default:
-                break;
-        }
         // Pass the event to ActionBarDrawerToggle, if it returns
         // true, then it has handled the app icon touch event
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void searchBox(boolean visibility){
-        if(visibility == true) {
-            mHideSearch = true;
-            invalidateOptionsMenu();
-            mSearchBox.setVisibility(View.VISIBLE);
-        }else {
-            mHideSearch = false;
-            invalidateOptionsMenu();
-            mSearchBox.setVisibility(View.INVISIBLE);
-        }
     }
 }
